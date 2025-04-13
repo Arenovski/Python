1aa21135bdc8b4a12222ba56822bc215e4e47b8a
Arenovski <beyzaolgu@gmail.com>
2025-04-13 18:57:43 +1000

    TypeCasting

---
+++ Data Types/TypeCasting.py
@@ -0,0 +1,15 @@
+Name = "Aren"
+print(type(Name))
+
+Age = 11
+print(type(Age))
+Age = float(Age)
+print(Age)
+
+Weight = 50.6
+print(type(Weight))
+Weight = int(Weight)
+print(Weight)
+
+Student = True
+print(type(Student))
