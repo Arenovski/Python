98bfbd58fded89dd16eff8150a8f853a324aa52b
Arenovski <beyzaolgu@gmail.com>
2025-04-26 19:00:17 +1000

    Percentages

---
+++ Python Operaters1/Percentage.py
@@ -0,0 +1,9 @@
+print("Enter the marks obtained")
+Maths = int(input("Enter your marks in maths"))
+English = int(input("Enter your marks in enlish"))
+History = int(input("Enter your marks in history"))
+Science = int(input("Enter your marks in science"))
+sum = Maths + English + History + Science
+print(sum)
+percentage = (sum/400)*100
+print(percentage)
