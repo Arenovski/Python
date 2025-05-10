cab7aa706b82ad7d7110d4f5cafaa7a0e0d1358b
Arenovski <beyzaolgu@gmail.com>
2025-05-10 19:11:37 +1000

    Or and and statments

---
+++ Python Operators 2/And or Operator.py
@@ -0,0 +1,12 @@
+A = 15
+B = -517935
+C = 39452
+
+if A>0 and C>0:
+    print("Both Number are greater than 0")
+else:
+    print("Both Numbers are less than 0")
+if A>0 or B>0:
+    print("Ethier is Greater than 0")
+else:
+    print("No Number is Greater than 0")
