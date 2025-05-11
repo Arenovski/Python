982a8f972cb9b47bc2ca07e56a4c6c7403ec54fd
Arenovski <beyzaolgu@gmail.com>
2025-05-11 18:54:51 +1000

    Identy Operators(:

---
+++ Python operators 3/Identity Operators.py
@@ -0,0 +1,12 @@
+#x = int, float(input("Enter a Number"))
+#if (type(x) is int):
+    #print("True")
+#else:
+    #print("False")
+
+P = int(input("Enter a Number"))
+N = int(input("Enter a Number"))
+if (P is not N):
+    print("P and N Have different identity")
+#else:
+    #print("P and N have different Identity")
