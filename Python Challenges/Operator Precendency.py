8de2e19e96bcb7ae4b749bcc5e4338a1215c5122
Arenovski <beyzaolgu@gmail.com>
2025-05-18 18:58:12 +1000

    Operator precendency

---
+++ Python Challenges/Operator Precendency.py
@@ -0,0 +1,14 @@
+v = 10
+a = 5
+d = 7
+g = 3
+l = 1
+result = (v*a)-2*g
+print(result)
+
+name = str(input("What is your name"))
+age = int(input("What is your age"))
+if name == "Aren" and age == 11:
+    print("Welcome")
+else:
+    print("Access Denied")
