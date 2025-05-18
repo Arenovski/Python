a55966fac7059c708405c1ace3e97b77d3fd51aa
Arenovski <beyzaolgu@gmail.com>
2025-05-18 19:07:59 +1000

    Divisible Numbers

---
+++ Python Challenges/Divisable Number.py
@@ -0,0 +1,8 @@
+print("Enter your Number")
+Numerator = int(input())
+print("Enter your Number")
+Denominator = int(input())
+if Numerator%Denominator == 0:
+    print("This Numerator is divisble by the Denominator")
+else:
+    print("This Numerator is not divisable by the denominator")
