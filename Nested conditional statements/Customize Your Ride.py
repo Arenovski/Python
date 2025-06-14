2b6a5d5d4e9e69a235dc0de0cc0dea4b187a8268
Arenovski <beyzaolgu@gmail.com>
2025-06-14 19:12:26 +1000

    Customize Your Ride

---
+++ Nested conditional statements/Customize Your Ride.py
@@ -0,0 +1,27 @@
+print("Choose Your Ride:  ")
+print("1. Car")
+print("2. Bike")
+
+choice = int(input("Enter your choice 1 or 2"))
+
+if(choice == 2):
+    print("What Type Of Bike?")
+    print("1. Scooter\n")
+    print("2. Bicycle\n")
+
+    choice2 = int(input("Enter your choice 1 or 2"))
+    if(choice2 == 1):
+      print("You Have Selected Scooter")
+    else:
+      print("You have Selected Bicycle")
+elif(choice == 1):
+  print("What Type Of Car?")
+  print("1.Mercedes")
+  print("2.BMW")
+  choice3 = int(input("Enter Your Choice"))
+  if(choice3 == 1):
+    print("You have chosen Mercedes")
+  else:
+    print("You Have Chosen BMW")
+else:
+  print("Invalid Answer")
