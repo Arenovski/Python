0101f07fc22909184722e451aee373e3bb1718ab
Arenovski <beyzaolgu@gmail.com>
2025-06-15 18:19:52 +1000

    Student Age Checker

---
+++ Homework/Student Entries.py
@@ -0,0 +1,8 @@
+Age = int(input("What Is Your Age"))
+if Age >= 10:
+    if Age <= 20:
+        print("Your Allowed")
+    else:
+        print("You are to old to enter")
+else:
+    print("You're to Young to Enter")
