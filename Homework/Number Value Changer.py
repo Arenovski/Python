98a224218c5489301afd7e754b090ce403d0bcab
Arenovski <beyzaolgu@gmail.com>
2025-06-15 18:26:44 +1000

    Value Changer

---
+++ Homework/Number Value Changer.py
@@ -0,0 +1,14 @@
+
+a = float, int(input("What is the first number you wrote"))
+b = float, int(input("What is the second number you wrote"))
+c = float, int(input("What is the third number you wrote"))
+
+
+original_a = b
+original_b = c
+original_c = a
+
+print("Fixed values:")
+print("a =", original_a)
+print("b =", original_b)
+print("c =", original_c)
